21bdbe9860b98d61dad19da4448c041e9afc0d10
fungusflip <filip.renemark@student.forsbergsskola.se>
2023-12-06 01:05:31 +0100

    b

--- Cats/Assets/Scripts/Character/Ivor.cs
+++ Cats/Assets/Scripts/Character/Ivor.cs
@@ -9,17 +9,26 @@ public class Ivor : MonoBehaviour
     private float runningAcceleration = 8f;
     private float currentSpeed;
     private bool isRunning = false;
+    private SpriteRenderer spriteRenderer;
+    private bool isFacingRight = true;
+    [SerializeField] private Animator animator;
     
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        FlipSpriteDirection(Input.GetAxis("Horizontal"));
         float dex = PlayerPrefs.GetFloat("DEX");
+        
+        animator.SetBool("Walk", isRunning);
+        animator.SetFloat("Speed", Mathf.Abs(currentSpeed));
         maxRunningSpeed = maxWalkingSpeed + Mathf.Floor(dex / 5);
 
         float targetSpeed = Mathf.Clamp(dex, 0f, isRunning ? maxRunningSpeed : maxWalkingSpeed);
@@ -42,6 +51,23 @@ public class Ivor : MonoBehaviour
             isRunning = false;
         }
         Debug.Log(currentSpeed);
+        Debug.Log(isRunning);
+    }
+    
+    
+    void FlipSpriteDirection(float horizontalInput)
+    {
+        // Check if the character is moving to the right or left
+        if (horizontalInput > 0 && !isFacingRight || horizontalInput < 0 && isFacingRight)
+        {
+            // Toggle the facing direction
+            isFacingRight = !isFacingRight;
+
+            // Flip the sprite by changing the localScale's x value
+            Vector3 newScale = transform.localScale;
+            newScale.x *= -1;
+            transform.localScale = newScale;
+        }
     }
 }
 
